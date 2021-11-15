13c8a4627dae9480080316ccd4732b9783b12c11
phil <philf7338@gmail.com>
2021-11-15 13:01:44 +0000

    new change

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer} from '@react-navigation/native';
+import { useNavigation} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+import SplashScreen from "./screens/SplashScreen"; 
+
 
 export default function App() {
+  const Nav= createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View  style={{flex:1}}>
+      <NavigationContainer>
+        <Nav.Navigator  >
+          <Nav.Screen style={styles.container}  name="SplashScreen" component={SplashScreen}/>
+          <Nav.Screen  name="LoginScreen" component={LoginScreen}/>
+          <Nav.Screen  name="HomeScreen" component={HomeScreen}/>
+        </Nav.Navigator>
+      </NavigationContainer>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ const styles = StyleSheet.create({
+   container: {
+   //  flex: 1,
+    backgroundColor: 'black',
+   alignItems: 'center',
+ justifyContent: 'center'
+   },
+ });
