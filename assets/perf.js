67877e65ffd4e4b85edc90a35b7266fb3d5ade02
phil <philf7338@gmail.com>
2021-11-17 22:51:08 +0000

    new change

---
+++ assets/perf.js
@@ -0,0 +1,54 @@
+const perf = [
+    {
+      id: 1,
+      name: 'Gucci Gold',
+      price: '39.99',
+      img: require('../assets/perf1.jpg'),
+      about:
+        'Gold is a hot floral fragrance, a bit sweet and with a dark background. Top notes: black plum, litchee, grapefruit; Heart: heliotrope;Base: benzoin, sandalwood, tonka bean..',
+    },
+  
+    {
+      id: 2,
+      name: 'Versache Eros',
+      price: '300.00',
+      img: require('../assets/perf2.jpg'),
+      about:
+        'Versache Eros contains mint, sea water and rosemary as top notes, lavender, jasmine, geranium and neroli as heart notes, and oakmoss, musk and sandalwood as base notes.',
+    },
+    {
+      id: 3,
+      name: 'Dolce & Gabanna Grey',
+      price: '150.00',
+      img: require('../assets/perf3.jpg'),
+      about:
+        'Grey of fresh and vibrant Orange immediately awaken the senses. A clear and sensual heart reveals the transparent accords of Grasse Jasmine and May Rose. The scent finally unfurls with the vibrant accents of Patchouli and Vetiver.',
+    },
+  
+    {
+      id: 4,
+      name: 'YvesSaintLaurent',
+      price: '200.00',
+      img: require('../assets/perf4.jpg'),
+      about:
+        'The opening notes of YSL are sweet and floral-infused dominated by the tea and bergamot fragrances. At the heart of the Flowerbomb fragrance are the floral notes which linger to create a long-lasting scent which creates an opulent and luxurious perfume that creates a floral aroma loved by millions',
+    },
+    {
+      id: 5,
+      name: 'Musk',
+      price: '250.99',
+      img: require('../assets/perf5.jpg'),
+      about:
+        'Musk disenchants the environment with a sweet, warm chocolate scent. The notes are reminiscent of milk chocolate (not bitter chocolate) with light vanilla accompaniment. Similar to a cup of chocolate or a glass of chocolate milk - which drinks can certainly provide moments of happiness',
+    },
+  {
+     id: 6,
+      name: 'Philty',
+      price: '550.99',
+      img: require('../assets/perf6.jpg'),
+      about:
+        'Philty Al Arabia Royal Blend Perfume is an Amber fruity oriental fragrance for women and men. The fragrance features a rich composition, good projection and longevity. Khaltaat Al Arabia Royal Blends is filled in an exclusive, depicting strength Fruity, Spicy and Woody notes from Lattafa is composed of quality with modern developments. What all everyone wants just one thing; to love the warm scent with exotic woods and spices? The perfume comes in a beautiful elegant bottle along with a nice outer box.',
+   },
+  ];
+  
+  export default perf;
